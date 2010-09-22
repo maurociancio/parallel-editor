16a71dd18dc66e6f8c80c1e78fef02aa0af5ae2f
Mauro Ciancio <maurociancio@gmail.com>
2010-09-22 15:44:24 -0300

    message with serializable annotation

--- src/parallel-editor-kernel/src/main/scala/ar/noxit/paralleleditor/kernel/messages/RemoteClientMessage.scala
+++ src/parallel-editor-kernel/src/main/scala/ar/noxit/paralleleditor/kernel/messages/RemoteClientMessage.scala
@@ -4,9 +4,21 @@ import scala.serializable
 
 @serializable
 case class RemoteLogin(val username: String)
+
+@serializable
 case class RemoteLogoutRequest
+
+@serializable
 case class RemoteLoginOkResponse
+
+@serializable
 case class RemoteNewDocumentRequest(val title: String)
+
+@serializable
 case class RemoteNewDocumentOkResponse
+
+@serializable
 case class RemoteDocumentList
+
+@serializable
 case class RemoteDocumentListResponse(val docList: List[String])
