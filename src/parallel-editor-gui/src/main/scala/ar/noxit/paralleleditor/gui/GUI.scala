fc4b58dfd5fe25a9f67db6defd2e3cbd9839f50d
Mauro Ciancio <maurociancio@gmail.com>
2010-09-22 15:36:21 -0300

    delete comments

--- src/parallel-editor-gui/src/main/scala/ar/noxit/paralleleditor/gui/GUI.scala
+++ src/parallel-editor-gui/src/main/scala/ar/noxit/paralleleditor/gui/GUI.scala
@@ -2,15 +2,6 @@ package ar.noxit.paralleleditor.gui
 
 import swing._
 
-/**
- * Created by IntelliJ IDEA.
- * User: legilioli
- * Date: 21/09/2010
- * Time: 17:57:37
- * To change this template use File | Settings | File Templates.
- */
-
-
 object GUI extends SimpleSwingApplication{
 
   def top = new MainFrame{
@@ -24,5 +15,4 @@ object GUI extends SimpleSwingApplication{
     }
 
   }
-
 }
