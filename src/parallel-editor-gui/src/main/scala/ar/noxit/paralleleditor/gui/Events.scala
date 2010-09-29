5f4c42c8ecb4965718967730eaa910479ea21ce3
Leandro Gilioli <legilioli@gmail.com>
2010-09-29 00:47:50 -0300

    agrego clases para los  eventos de insercion y eliminacion en el editor

--- src/parallel-editor-gui/src/main/scala/ar/noxit/paralleleditor/gui/Events.scala
+++ src/parallel-editor-gui/src/main/scala/ar/noxit/paralleleditor/gui/Events.scala
@@ -2,4 +2,7 @@ package ar.noxit.paralleleditor.gui
 
 import swing.event.Event
 
+
 case class ConnectionRequest(val host: String, val port: Integer) extends Event
+case class InsertionEvent(val pos:Integer, val text:String) extends Event
+case class DeletionEvent(val pos:Integer, count:Integer) extends Event
