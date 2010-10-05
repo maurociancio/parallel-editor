ffab8c2dab256c49ab3770c4c6548ab8a1f49b40
Mauro Ciancio <maurociancio@gmail.com>
2010-10-05 17:25:42 -0300

    juntar mensajes remotos

--- src/parallel-editor-common/src/main/scala/ar/noxit/paralleleditor/common/messages/RemoteAddText.scala
+++
@@ -1,9 +0,0 @@
-package ar.noxit.paralleleditor.common.messages
-
-import scala.serializable
-
-@serializable
-abstract case class RemoteOperation
-
-@serializable
-case class RemoteAddText(val text: String, val startPos: Int) extends RemoteOperation
--- src/parallel-editor-common/src/main/scala/ar/noxit/paralleleditor/common/messages/RemoteDeleteText.scala
+++
@@ -1,6 +0,0 @@
-package ar.noxit.paralleleditor.common.messages
-
-import scala.serializable
-
-@serializable
-case class RemoteDeleteText(val startPos: Int, val size: Int) extends RemoteOperation
