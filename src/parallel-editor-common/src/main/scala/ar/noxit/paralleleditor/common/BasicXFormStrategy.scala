dad4e12e7c23cbf033bc86a1e0d769a56acefccf
Mauro Ciancio <maurociancio@gmail.com>
2010-10-07 13:08:45 -0300

    refactor xform

--- src/parallel-editor-common/src/main/scala/ar/noxit/paralleleditor/common/BasicXFormStrategy.scala
+++ src/parallel-editor-common/src/main/scala/ar/noxit/paralleleditor/common/BasicXFormStrategy.scala
@@ -3,56 +3,74 @@ package ar.noxit.paralleleditor.common
 import operation.{CompositeOperation, EditOperation, DeleteTextOperation, AddTextOperation}
 
 class BasicXFormStrategy extends XFormStrategy {
-    def xform(ops: (EditOperation, EditOperation)) = {
+    override def xform(ops: (EditOperation, EditOperation)) = {
         if (ops == null)
             throw new IllegalArgumentException("ops cannot be null")
 
         ops match {
-            case (c: AddTextOperation, s: AddTextOperation) => {
-                if (c.startPos == s.startPos)
-                    (new AddTextOperation(c.text, c.startPos), new AddTextOperation(s.text, c.startPos + c.text.length))
-                else if (c.startPos < s.startPos)
-                    (c, new AddTextOperation(s.text, s.startPos + c.text.length))
-                else
-                    (new AddTextOperation(c.text, c.startPos + s.text.length), s)
-            }
+            case (c: AddTextOperation, s: AddTextOperation) =>
+                xform(c, s)
+            case (c: DeleteTextOperation, s: DeleteTextOperation) =>
+                xform(c, s)
+            case (c: AddTextOperation, s: DeleteTextOperation) =>
+                xform(c, s)
             case (c: DeleteTextOperation, s: AddTextOperation) => {
                 val res = xform(s, c)
                 (res._2, res._1)
             }
-            case (c: DeleteTextOperation, s: DeleteTextOperation) => {
-                val intersection = getRangeFor(c) intersect getRangeFor(s)
+        }
+    }
 
-                val min = if (c.startPos > s.startPos) s else c
-                val max = if (c.startPos <= s.startPos) s else c
+    /**
+     * Caso agregar-agregar
+     */
+    protected def xform(c: AddTextOperation, s: AddTextOperation): (AddTextOperation, AddTextOperation) = {
+        if (c.startPos == s.startPos)
+            (new AddTextOperation(c.text, c.startPos), new AddTextOperation(s.text, c.startPos + c.text.length))
+        else if (c.startPos < s.startPos)
+            (c, new AddTextOperation(s.text, s.startPos + c.text.length))
+        else
+            (new AddTextOperation(c.text, c.startPos + s.text.length), s)
+    }
 
-                if (intersection.isEmpty) {
-                    val dt = new DeleteTextOperation(max.startPos - min.size, max.size)
-                    if (min == c)
-                        (min, dt)
-                    else
-                        (dt, min)
-                } else {
-                    val noSuperpuestosDeMin = getRangeFor(min) diff intersection
-                    val noSuperpuestosDeMax = getRangeFor(max) diff intersection
+    /**
+     * Caso borrar-borrara
+     */
+    protected def xform(c: DeleteTextOperation, s: DeleteTextOperation): (DeleteTextOperation, DeleteTextOperation) = {
+        val intersection = getRangeFor(c) intersect getRangeFor(s)
 
-                    (new DeleteTextOperation(min.startPos, noSuperpuestosDeMin.size), new DeleteTextOperation(min.startPos, noSuperpuestosDeMax.size))
-                }
-            }
-            case (c: AddTextOperation, s: DeleteTextOperation) => {
-                val deletionRange = getRangeFor(s)
-                if (deletionRange contains c.startPos) {
-                    (new AddTextOperation(c.text, s.startPos),
-                            new CompositeOperation(
-                                new DeleteTextOperation(s.startPos, (c.startPos - s.startPos)),
-                                new DeleteTextOperation(s.startPos + c.text.length, s.startPos + s.size - c.startPos)))
-                } else {
-                    if (c.startPos < s.startPos)
-                        (c, new DeleteTextOperation(s.startPos + c.text.length, s.size))
-                    else
-                        (new AddTextOperation(c.text, c.startPos - s.size), s)
-                }
-            }
+        val min = if (c.startPos > s.startPos) s else c
+        val max = if (c.startPos <= s.startPos) s else c
+
+        if (intersection.isEmpty) {
+            val dt = new DeleteTextOperation(max.startPos - min.size, max.size)
+            if (min == c)
+                (min, dt)
+            else
+                (dt, min)
+        } else {
+            val noSuperpuestosDeMin = getRangeFor(min) diff intersection
+            val noSuperpuestosDeMax = getRangeFor(max) diff intersection
+
+            (new DeleteTextOperation(min.startPos, noSuperpuestosDeMin.size), new DeleteTextOperation(min.startPos, noSuperpuestosDeMax.size))
+        }
+    }
+
+    /**
+     * Caso agregar-borrar
+     */
+    protected def xform(c: AddTextOperation, s: DeleteTextOperation): (AddTextOperation, EditOperation) = {
+        val deletionRange = getRangeFor(s)
+        if (deletionRange contains c.startPos) {
+            (new AddTextOperation(c.text, s.startPos),
+                    new CompositeOperation(
+                        new DeleteTextOperation(s.startPos, (c.startPos - s.startPos)),
+                        new DeleteTextOperation(s.startPos + c.text.length, s.startPos + s.size - c.startPos)))
+        } else {
+            if (c.startPos < s.startPos)
+                (c, new DeleteTextOperation(s.startPos + c.text.length, s.size))
+            else
+                (new AddTextOperation(c.text, c.startPos - s.size), s)
         }
     }
 
--- src/parallel-editor-common/src/main/scala/ar/noxit/paralleleditor/common/operation/AddTextOperation.scala
+++ src/parallel-editor-common/src/main/scala/ar/noxit/paralleleditor/common/operation/AddTextOperation.scala
@@ -1,12 +1,10 @@
 package ar.noxit.paralleleditor.common.operation
 
-
 class AddTextOperation(val text: String, val startPos: Int) extends EditOperation {
 
     def executeOn(documentData: DocumentData) = {
         val original = documentData.data
         documentData.data = original.substring(0, startPos) + text + original.substring(startPos)
-        println(documentData.data)
     }
 
     override def toString = {
