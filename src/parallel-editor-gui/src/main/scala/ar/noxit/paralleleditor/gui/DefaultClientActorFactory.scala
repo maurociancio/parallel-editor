223d61f4a6db2823d770a3c978009b19d680b789
Mauro Ciancio <maurociancio@gmail.com>
2010-10-18 23:11:08 -0300

    refactor gui

---
+++ src/parallel-editor-gui/src/main/scala/ar/noxit/paralleleditor/gui/DefaultClientActorFactory.scala
@@ -0,0 +1,17 @@
+package ar.noxit.paralleleditor.gui
+
+import java.net.Socket
+import ar.noxit.paralleleditor.common.network.SocketNetworkConnection
+import remotes.RemoteServerProxy
+
+class DefaultClientActorFactory extends ClientActorFactory {
+    def newActor(host: String, port: Int, docs: Documents) = {
+        val socket = new Socket(host, port)
+        val factory = new GuiActorFactory(docs)
+
+        // TODO resolver el tema de la conexión, que se cierra on disconnect
+        new RemoteServerProxy(new SocketNetworkConnection(socket), factory)
+
+        factory.guiActor
+    }
+}
--- src/parallel-editor-gui/src/main/scala/ar/noxit/paralleleditor/gui/GUI.scala
+++ src/parallel-editor-gui/src/main/scala/ar/noxit/paralleleditor/gui/GUI.scala
@@ -1,30 +1,14 @@
 package ar.noxit.paralleleditor.gui
 
-import remotes.RemoteServerProxy
 import scala.swing._
-import java.net.Socket
 import scala.actors.Actor
 import ar.noxit.paralleleditor.common.logger.Loggable
-import ar.noxit.paralleleditor.common.network.SocketNetworkConnection
 import ar.noxit.paralleleditor.common.messages._
 import swing.TabbedPane.Page
 import ar.noxit.paralleleditor.common.converter._
 
-class DocumentsAdapter(private val tabs: TabbedPane,
-                       private val menu: HomeMenuBar,
-                       private val gui: Reactor) extends Documents {
-    override def changeDocList(l: List[String]) = menu changeDocList l
-
-    override def byName(title: String) = {
-        val page = tabs.pages.find {page => page.title == title}
-        page.map {p => p.content.asInstanceOf[DocumentArea]}
-    }
-
-    def createDocument(title: String, content: String) {
-        val doc = new DocumentArea(title, content)
-        gui.listenTo(doc)
-        tabs.pages += new Page(title, doc)
-    }
+trait ClientActorFactory {
+    def newActor(host: String, port: Int, docs: Documents): Actor
 }
 
 object GUI extends SimpleSwingApplication with Loggable {
@@ -33,6 +17,9 @@ object GUI extends SimpleSwingApplication with Loggable {
 
     // TODO INYECTAR
     val converter = new DefaultRemoteDocumentOperationConverter(new DefaultSyncOperationConverter(new DefaultEditOperationConverter))
+
+    // TODO inyectar
+    val clientActorFactory = new DefaultClientActorFactory
 
     def top = new MainFrame {
         title = "Parallel Editor GUI"
@@ -59,11 +46,7 @@ object GUI extends SimpleSwingApplication with Loggable {
                 trace("Connecting to %s %s", host, port)
 
                 connected = true
-                val socket = new Socket(host, port.intValue)
-                val factory = new GuiActorFactory(documents)
-                new RemoteServerProxy(new SocketNetworkConnection(socket), factory)
-
-                actor = factory.guiActor
+                actor = clientActorFactory.newActor(host, port.intValue, documents)
                 actor ! RemoteLoginRequest(connPanel user)
             }
 
