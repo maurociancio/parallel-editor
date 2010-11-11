e7ebdea0465d47e7530f2f68e95efaa668549fda
Mauro Ciancio <maurociancio@gmail.com>
2010-11-11 16:47:27 -0300

    null kernel activator

---
+++ src/parallel-editor-kernel/src/main/scala/ar/noxit/paralleleditor/kernel/Activator.scala
@@ -0,0 +1,11 @@
+package ar.noxit.paralleleditor.kernel
+
+import org.osgi.framework.{BundleContext, BundleActivator}
+
+class Activator extends BundleActivator {
+    def stop(ctx: BundleContext) = {
+    }
+
+    def start(ctx: BundleContext) = {
+    }
+}
