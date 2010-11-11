ba10e817a8fa415ac6f7aa5cce30d4e22f6d6756
Mauro Ciancio <maurociancio@gmail.com>
2010-11-11 16:51:00 -0300

    osgi para proyecto cliente

---
+++ src/parallel-editor-client/src/main/scala/ar/noxit/paralleleditor/client/Activator.scala
@@ -0,0 +1,11 @@
+package ar.noxit.paralleleditor.client
+
+import org.osgi.framework.{BundleContext, BundleActivator}
+
+class Activator extends BundleActivator {
+    def start(ctx: BundleContext) = {
+    }
+
+    def stop(ctx: BundleContext) = {
+    }
+}
