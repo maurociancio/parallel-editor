d4955beff22efc746875e58662a5153470534a17
Mauro Ciancio <maurociancio@gmail.com>
2010-11-12 15:31:29 -0300

    agregar tests a plugin eclipse

---
+++ src/parallel-editor-eclipse/test/ar/noxit/paralleleditor/eclipse/ShareDocumentActionTest.java
@@ -0,0 +1,15 @@
+package ar.noxit.paralleleditor.eclipse;
+
+import org.easymock.EasyMock;
+import org.eclipse.jface.text.IDocument;
+import org.testng.annotations.Test;
+
+@Test
+public class ShareDocumentActionTest {
+
+	@Test
+	public void lal() {
+		EasyMock.createMock(IDocument.class);
+		System.out.println("");
+	}
+}
