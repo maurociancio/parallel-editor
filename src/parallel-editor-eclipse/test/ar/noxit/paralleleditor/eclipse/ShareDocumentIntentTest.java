597b39d6d182243d1fec6ea650916ed80f8c6069
Mauro Ciancio <maurociancio@gmail.com>
2010-11-12 16:42:38 -0300

    test share document intent

--- src/parallel-editor-eclipse/test/ar/noxit/paralleleditor/eclipse/ShareDocumentIntentTest.java
+++ src/parallel-editor-eclipse/test/ar/noxit/paralleleditor/eclipse/ShareDocumentIntentTest.java
@@ -2,8 +2,12 @@ package ar.noxit.paralleleditor.eclipse;
 
 import org.easymock.EasyMock;
 import org.eclipse.core.filebuffers.LocationKind;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import ar.noxit.paralleleditor.eclipse.infrastructure.share.AbstractShareDocumentIntent;
@@ -11,6 +15,13 @@ import ar.noxit.paralleleditor.eclipse.infrastructure.share.ITextFileManager;
 
 @Test
 public class ShareDocumentIntentTest {
+
+	private int count;
+
+	@BeforeMethod
+	public void before() {
+		count = 0;
+	}
 
 	@Test
 	public void testShareDocument() throws Exception {
@@ -29,5 +40,31 @@ public class ShareDocumentIntentTest {
 
 		shareDocumentIntent.shareDocument(path, LocationKind.IFILE);
 		EasyMock.verify(fileBuffer);
+	}
+
+	@Test
+	public void testShareDocumentWithException() throws Exception {
+		Path path = new Path("/");
+
+		final ITextFileManager fileBuffer = EasyMock.createMock(ITextFileManager.class);
+		fileBuffer.connect(EasyMock.eq(path), EasyMock.eq(LocationKind.IFILE), (IProgressMonitor) EasyMock.anyObject());
+		EasyMock.expectLastCall().andThrow(new CoreException(new Status(0, "data", "data")));
+		EasyMock.replay(fileBuffer);
+
+		AbstractShareDocumentIntent shareDocumentIntent = new AbstractShareDocumentIntent() {
+			@Override
+			protected ITextFileManager getTextFileBufferManager() {
+				return fileBuffer;
+			}
+
+			@Override
+			protected void onException(CoreException e) {
+				count = count + 1;
+			}
+		};
+
+		shareDocumentIntent.shareDocument(path, LocationKind.IFILE);
+		EasyMock.verify(fileBuffer);
+		Assert.assertEquals(1, count);
 	}
 }
