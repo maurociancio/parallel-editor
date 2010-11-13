3e80201e0f20119e1aad7c1ac4c37b1f992831ea
Mauro Ciancio <maurociancio@gmail.com>
2010-11-13 19:37:01 -0300

    copy libs script

---
+++ src/copy_libs_to_eclipse_plugin.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp parallel-editor-client/target/parallel-editor-client-1.0-SNAPSHOT.jar parallel-editor-eclipse/libs/
+cp parallel-editor-kernel/target/parallel-editor-kernel-1.0-SNAPSHOT.jar parallel-editor-eclipse/libs/
