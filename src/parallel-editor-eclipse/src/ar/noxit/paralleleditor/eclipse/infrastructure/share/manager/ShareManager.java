31d55ed8a0a73f1d50611ec0d05fbd0bd67720c0
Mauro Ciancio <maurociancio@gmail.com>
2010-11-13 20:37:15 -0300

    crear kernel service desde eclipse

--- src/parallel-editor-eclipse/src/ar/noxit/paralleleditor/eclipse/infrastructure/share/manager/ShareManager.java
+++ src/parallel-editor-eclipse/src/ar/noxit/paralleleditor/eclipse/infrastructure/share/manager/ShareManager.java
@@ -2,7 +2,19 @@ package ar.noxit.paralleleditor.eclipse.infrastructure.share.manager;
 
 import org.eclipse.core.runtime.Assert;
 
+import ar.noxit.paralleleditor.client.CommandFromKernel;
+import ar.noxit.paralleleditor.client.Documents;
+import ar.noxit.paralleleditor.client.JSession;
+import ar.noxit.paralleleditor.client.SessionFactory;
+import ar.noxit.paralleleditor.common.BasicXFormStrategy;
+import ar.noxit.paralleleditor.common.messages.RemoteLoginRequest;
 import ar.noxit.paralleleditor.eclipse.infrastructure.share.IShareManager;
+import ar.noxit.paralleleditor.kernel.Kernel;
+import ar.noxit.paralleleditor.kernel.basic.BasicKernel;
+import ar.noxit.paralleleditor.kernel.basic.SynchronizerFactory;
+import ar.noxit.paralleleditor.kernel.basic.UserListMerger;
+import ar.noxit.paralleleditor.kernel.basic.sync.SynchronizerAdapterFactory;
+import ar.noxit.paralleleditor.kernel.basic.userlist.DefaultUserListMerger;
 import ar.noxit.paralleleditor.kernel.remote.KernelService;
 import ar.noxit.paralleleditor.kernel.remote.SocketKernelService;
 
@@ -13,13 +25,47 @@ public class ShareManager implements IShareManager {
 	@Override
 	public void createShare(String docTitle) {
 		Assert.isNotNull(docTitle);
+
+		createOrGetKernelService();
+		JSession serverSession = SessionFactory.newJSession("localhost", 5000, new Documents() {
+
+			@Override
+			public void process(CommandFromKernel command) {
+			}
+		});
+		serverSession.send(new RemoteLoginRequest("becho"));
 	}
 
 	protected KernelService createOrGetKernelService() {
-		if (kernelService == null) {
-			kernelService = new SocketKernelService(5000);
-			kernelService.startService();
+		if (this.kernelService == null) {
+			SocketKernelService kernelService = new SocketKernelService(5000);
+			kernelService.setKernel(newKernel());
+
+			this.kernelService = kernelService;
+			this.kernelService.startService();
 		}
-		return kernelService;
+		return this.kernelService;
+	}
+
+	protected Kernel newKernel() {
+		BasicKernel basicKernel = new BasicKernel();
+		basicKernel.setTimeout(5000);
+		basicKernel.setSync(newSyncFactory());
+		basicKernel.setUserListMerger(newUserListMerger());
+
+		return basicKernel;
+	}
+
+	protected SynchronizerFactory newSyncFactory() {
+		SynchronizerAdapterFactory synchronizerAdapterFactory = new SynchronizerAdapterFactory();
+		synchronizerAdapterFactory.setStrategy(new BasicXFormStrategy());
+
+		return synchronizerAdapterFactory;
+	}
+
+	protected UserListMerger newUserListMerger() {
+		DefaultUserListMerger defaultUserListMerger = new DefaultUserListMerger();
+		defaultUserListMerger.setTimeout(5000);
+		return defaultUserListMerger;
 	}
 }
