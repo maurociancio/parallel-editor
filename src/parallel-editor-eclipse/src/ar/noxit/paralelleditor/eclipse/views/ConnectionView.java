79d1b91da6e08a7413ff484a887a19c8da11826b
Leandro E. Gilioli <legilioli@gmail.com>
2010-11-12 21:20:50 -0300

    remove imports

--- src/parallel-editor-eclipse/src/ar/noxit/paralelleditor/eclipse/views/ConnectionView.java
+++ src/parallel-editor-eclipse/src/ar/noxit/paralelleditor/eclipse/views/ConnectionView.java
@@ -7,8 +7,6 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 
