16512a929ed30dd05d0a71d8a6fe587f0e3da2a0
Mauro Ciancio <maurociancio@gmail.com>
2010-11-17 22:27:11 -0300

    borrar codigo que no se usa

--- src/parallel-editor-eclipse/src/ar/noxit/paralleleditor/eclipse/views/TreeLabelProvider.java
+++ src/parallel-editor-eclipse/src/ar/noxit/paralleleditor/eclipse/views/TreeLabelProvider.java
@@ -1,14 +1,8 @@
 package ar.noxit.paralleleditor.eclipse.views;
 
 import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.swt.graphics.Image;
 
 public class TreeLabelProvider extends LabelProvider {
-
-	@Override
-	public Image getImage(Object element) {
-		return null;
-	}
 
 	@Override
 	public String getText(Object element) {
