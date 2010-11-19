9cd36a691c6eb7655ac7b8766b2cd39f07e0454b
Mauro Ciancio <maurociancio@gmail.com>
2010-11-19 13:49:47 -0300

    marcar la conexion  local en listado de hosts

--- src/parallel-editor-eclipse/src/ar/noxit/paralleleditor/eclipse/views/ConnectionInfoLabelProvider.java
+++ src/parallel-editor-eclipse/src/ar/noxit/paralleleditor/eclipse/views/ConnectionInfoLabelProvider.java
@@ -6,7 +6,11 @@ public class ConnectionInfoLabelProvider extends LabelProvider {
 
 	public String getText(Object element) {
 		ConnectionInfo h = (ConnectionInfo) element;
-		return element == null ? "" : h.getId().getHost() + ":" + h.getId().getPort() + " as "
-				+ h.getUsername();
+		if (element == null)
+			return null;
+
+		ConnectionId id = h.getId();
+		String isLocal = id.isLocal() ? "[X]" : "";
+		return String.format("%s %s:%d as %s", isLocal, id.getHost(), id.getPort(), h.getUsername());
 	}
 }
