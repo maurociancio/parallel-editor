33410026eaafe60b31e54921beae377859bf0e99
Mauro Ciancio <maurociancio@gmail.com>
2010-11-22 20:50:31 -0300

    chat view

---
+++ src/parallel-editor-eclipse/src/ar/noxit/paralleleditor/eclipse/views/ChatView.java
@@ -0,0 +1,15 @@
+package ar.noxit.paralleleditor.eclipse.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ChatView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+	}
+
+	@Override
+	public void setFocus() {
+	}
+}
