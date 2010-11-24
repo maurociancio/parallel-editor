0471c7c3b0c8f7fa3dc7b6ddd2dfd686f20cdb69
Mauro Ciancio <maurociancio@gmail.com>
2010-11-24 18:30:04 -0300

    agregar hora y fecha a log

--- src/parallel-editor-eclipse/src/ar/noxit/paralleleditor/eclipse/views/HistoryCallback.java
+++ src/parallel-editor-eclipse/src/ar/noxit/paralleleditor/eclipse/views/HistoryCallback.java
@@ -1,5 +1,6 @@
 package ar.noxit.paralleleditor.eclipse.views;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.eclipse.swt.widgets.Display;
@@ -10,16 +11,16 @@ import ar.noxit.paralleleditor.eclipse.infrastructure.share.manager.ISession.ICh
 public class HistoryCallback implements IChatCallback {
 
 	private final Text history;
-	private static final String CHAT_MESSAGE = "%s said (from %s): %s";
-	private static final String NEW_SUBSCRIBER_MESSAGE = "** New subscriber '%s' on document '%s'.";
-	private static final String SUBSCRIBER_LEFT_MESSAGE = "** Subscriber '%s' left document '%s'.";
-	private static final String LOGIN_MESSAGE = "** User '%s' has logged in.";
-	private static final String LOGOUT_MESSAGE = "** User '%s' has logged out.";
+	private static final String CHAT_MESSAGE = "%s %s said (from %s): %s";
+	private static final String NEW_SUBSCRIBER_MESSAGE = "** %s New subscriber '%s' on document '%s'.";
+	private static final String SUBSCRIBER_LEFT_MESSAGE = "** %s Subscriber '%s' left document '%s'.";
+	private static final String LOGIN_MESSAGE = "** %s User '%s' has logged in.";
+	private static final String LOGOUT_MESSAGE = "** %s User '%s' has logged out.";
 
-	private static final String LOCAL_CHAT_MESSAGE = "you said (to %s): %s";
+	private static final String LOCAL_CHAT_MESSAGE = "%s you said (to %s): %s";
 
 	public static String formatLocalChat(Date now, String chat, ConnectionInfo info) {
-		return String.format(LOCAL_CHAT_MESSAGE, getStringOf(info), chat) + "\n";
+		return "\n" + String.format(LOCAL_CHAT_MESSAGE, formatTime(now), getStringOf(info), chat);
 	}
 
 	public HistoryCallback(Text history) {
@@ -28,31 +29,35 @@ public class HistoryCallback implements IChatCallback {
 
 	@Override
 	public void onNewChat(Date when, ConnectionInfo from, String username, String chat) {
-		append(String.format(CHAT_MESSAGE, username, getStringOf(from), chat));
+		append(String.format(CHAT_MESSAGE, formatTime(when), username, getStringOf(from), chat));
 	}
 
 	@Override
 	public void onNewSubscriber(Date when, String username, String docTitle) {
-		append(String.format(NEW_SUBSCRIBER_MESSAGE, username, docTitle));
+		append(String.format(NEW_SUBSCRIBER_MESSAGE, formatTime(when), username, docTitle));
 	}
 
 	@Override
 	public void onSubscriberLeft(Date when, String username, String docTitle) {
-		append(String.format(SUBSCRIBER_LEFT_MESSAGE, username, docTitle));
+		append(String.format(SUBSCRIBER_LEFT_MESSAGE, formatTime(when), username, docTitle));
 	}
 
 	@Override
 	public void onNewLogin(Date when, String username) {
-		append(String.format(LOGIN_MESSAGE, username));
+		append(String.format(LOGIN_MESSAGE, formatTime(when), username));
 	}
 
 	@Override
 	public void onNewLogout(Date date, String username) {
-		append(String.format(LOGOUT_MESSAGE, username));
+		append(String.format(LOGOUT_MESSAGE, formatTime(date), username));
 	}
 
 	private static String getStringOf(ConnectionInfo current) {
 		return current.getId().getHost() + ":" + current.getId().getPort();
+	}
+
+	private static String formatTime(Date date) {
+		return "[" + new SimpleDateFormat("HH:mm:ss").format(date) + "]";
 	}
 
 	private void append(final String text) {
@@ -60,7 +65,7 @@ public class HistoryCallback implements IChatCallback {
 
 			@Override
 			public void run() {
-				history.append(text + "\n");
+				history.append("\n" + text);
 			}
 		});
 	}
